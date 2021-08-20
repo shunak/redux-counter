2d1cd7e844962bbe9fbe6a243bbb26e8180eb727
shunak <shuna966@gmail.com>
2021-08-20 17:58:39 +0900

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,55 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import  ReactDOM  from "react-dom";
+import { Provider } from "react-redux";
+import { useDispatch,  useSelector } from "react-redux";
+import { createStore } from "redux";
+import { RootState } from "./store";
 
-function App() {
+// Component for Counter Application
+const App = () => {
+  // Call hook to Dispatch from ract-redux
+  const dispatch = useDispatch();
+  // Abstruct state by useSelector() from react-redux
+  const count = useSelector((state: RootState) => state.count);
+  // Event to call Dispatch function when push button
+  const handleAddCount = () => {
+    // Pass Action to Dispatch function, usually, Action is defined by object type value which has "type" as a key value. 
+    dispatch({type: "add_count"});
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <button onClick={handleAddCount}>+1</button>
+      <p>{count}</p>
     </div>
-  );
+  )
 }
+
+// Set initial state
+const initialState = { count: 0 };
+// Create Reducer function, counter for Action
+const counter = (state = initialState, action: any) => {
+ switch(action.type) {
+    // In case "add_count" Action is published, update state and return it.
+    case "add_count":
+      const newCount = state.count + 1;
+      return {
+        count: newCount        
+     }; 
+     default:
+       return state;
+  }
+};
+
+// // Pass the Reducer(counter) to createStore() function
+// const store = createStore(counter);
+// // Redux is valid by Folding App component by Provider.
+// // Pass the store value created by createStore() function.
+// ReactDOM.render(
+//   <Provider store={store}>
+//       <App />
+//   </Provider>,
+//   document.getElementById("root"),
+// );
+
 
 export default App;
--- src/index.tsx
+++ src/index.tsx
@@ -1,13 +1,36 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from "react-redux";
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { createStore } from "redux";
+
+// Set initial state
+const initialState = { count: 0 };
+// Create Reducer function, counter for Action
+const counter = (state = initialState, action: any) => {
+ switch(action.type) {
+    // In case "add_count" Action is published, update state and return it.
+    case "add_count":
+      const newCount = state.count + 1;
+      return {
+        count: newCount        
+     }; 
+     default:
+       return state;
+  }
+};
+// Pass the Reducer(counter) to createStore() function
+const store = createStore(counter);
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <Provider store={store}>
+      <App />
+  </Provider>,
+  // <React.StrictMode>
+  //   <App />
+  // </React.StrictMode>,
   document.getElementById('root')
 );
 
